1d7189d957236b84f097ff4ffd29f45de22450b2
faraday58 <jarodriguezvera@gmail.com>
2020-04-27 12:37:29 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OrdenaInternos;
+
+namespace AlgoritmosOrdenamiento
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] A = { 5, 2, 7, 9, 1 };
+
+            Interno inburbuja = new Interno();
+            inburbuja.Burbuja(A);
+            Console.WriteLine(inburbuja);
+
+
+        }
+    }
+}
